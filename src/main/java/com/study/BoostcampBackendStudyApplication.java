47529f58c05c34d1978b4c8867dbc51a6611050c
pkgonan <pkgonan1@naver.com>
2017-05-23 19:16:52 +0900

    Slf4j + Logback Logging

--- src/main/java/com/study/BoostcampBackendStudyApplication.java
+++ src/main/java/com/study/BoostcampBackendStudyApplication.java
@@ -1,12 +1,16 @@
 package com.study;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BoostcampBackendStudyApplication {
+	private static Logger logger = LoggerFactory.getLogger(BoostcampBackendStudyApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoostcampBackendStudyApplication.class, args);
+		logger.info("hello");
 	}
 }
