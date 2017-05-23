c868b9f6bad77ecd6f70f2d4acc731d0bb31df70
pkgonan <pkgonan1@naver.com>
2017-05-23 18:03:42 +0900

    JUnit Life Cycle Test

--- src/test/java/com/study/BoostcampBackendStudyApplicationTests.java
+++ src/test/java/com/study/BoostcampBackendStudyApplicationTests.java
@@ -1,5 +1,6 @@
 package com.study;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,9 +9,20 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BoostcampBackendStudyApplicationTests {
+	private int cnt = 0;
 
-	@Test
-	public void contextLoads() {
+	@Before
+	public void before(){
+		System.out.println(++cnt);
 	}
 
+	@Test
+	public void test1() {
+		System.out.println(++cnt);
+	}
+
+	@Test
+	public void test2(){
+		System.out.println(++cnt);
+	}
 }
