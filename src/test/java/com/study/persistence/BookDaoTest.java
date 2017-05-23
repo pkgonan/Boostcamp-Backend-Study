a849f272c47509319b5dfca09dd084ed8a66e75d
pkgonan <pkgonan1@naver.com>
2017-05-24 01:24:37 +0900

    통합 테스트

---
+++ src/test/java/com/study/persistence/BookDaoTest.java
@@ -0,0 +1,39 @@
+package com.study.persistence;
+
+import com.study.AppConfig;
+import com.study.domain.Book;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class BookDaoTest {
+    @Autowired
+    private BookDao dao;
+
+    @Test
+    public void shouldCount(){
+        int count = dao.countBooks();
+        System.out.println(count);
+    }
+
+    @Test
+    public void shouldInsertAndSelect(){
+        // given
+        Book book = new Book("Java 웹개발", "네이버", 342);
+
+        // when
+        Integer id = dao.insert(book);
+
+        // then
+        Book selected = dao.selectById(id);
+        System.out.println(selected);
+        assertThat(selected.getTitle(), is("Java 웹개발"));
+    }
+}
